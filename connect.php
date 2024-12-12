eed9908949f6b71ac1b998ac9529feb63f4f9b39
Alireza Ghorbani <77077960+ARGhorbani@users.noreply.github.com>
2024-12-12 17:05:22 +0330

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,11 @@
 <?php
-$server="autorack.proxy.rlwy.net:42793";
-$user="root";
-$pass="WDgBKrvdinmwHuZNxWgIDGQDBnhtEssd";
-$dbname="railway";
+$server = getenv('DB_HOST');
+$user = getenv('DB_USER');   
+$pass = getenv('DB_PASS');  
+$dbname = getenv('DB_NAME'); 
 
 $dsn="mysql:host=$server;dbname=$dbname";
+
 try{
 $connect=new PDO($dsn,$user,$pass);
 $connect->exec("SET character_set_connection = 'utf8'");
